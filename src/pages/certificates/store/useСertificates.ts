18b9b3fec348b2efc6e0c091b4f498919c5b0a7b
Илья Орешкин <ioreshkin@outlook.com>
2024-06-07 18:52:57 +0300

    Добавил types, store в сертификаты

---
+++ src/pages/certificates/store/useСertificates.ts
@@ -0,0 +1,21 @@
+import { axios } from '@/commom/axios'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { Certificate } from '../types'
+
+export const useСertificates = defineStore('сertificates', () => {
+  const list = ref<Certificate>()
+
+  const find = async () => {
+    await axios
+      .get('/сertificates')
+      .then((response) => {
+        list.value = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  return { find, list }
+})
---
+++ src/pages/certificates/types/index.ts
@@ -0,0 +1,10 @@
+interface Certificate {
+  id: number
+  name: string
+  description: string
+  image: string
+  status: boolean
+  wide: boolean
+}
+
+export type { Certificate }
