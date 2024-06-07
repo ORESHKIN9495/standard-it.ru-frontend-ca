43de654e5da04a3115d60cfea05caae2d7cbfba3
Илья Орешкин <ioreshkin@outlook.com>
2024-06-07 18:45:14 +0300

    Добавил types, store в карту партнера

---
+++ src/pages/card-partner/store/index.ts
@@ -0,0 +1,21 @@
+import { axios } from '@/commom/axios'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { CardPartner } from '../types'
+
+export const useCardPartners = defineStore('card-partners', () => {
+  const list = ref<CardPartner>()
+
+  const find = async () => {
+    await axios
+      .get('/partner-card')
+      .then((response) => {
+        list.value = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  return { find, list }
+})
---
+++ src/pages/card-partner/types/index.ts
@@ -0,0 +1,22 @@
+interface CardPartner {
+  id: number
+  inn: string
+  address: string
+  postal_address: string
+  delivery_note: string
+  bank_details: string
+  ogrn: string
+  okpo: string
+  okogu: string
+  okato: string
+  oktmo: string
+  okfs: string
+  okopf: string
+  okved: string
+  principal: string
+  phone: string
+  email: string
+  site: string
+}
+
+export type { CardPartner }
