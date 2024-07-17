d1585c0c3a5859cb6c38d5c129501b37ea21e368
Илья Орешкин <ioreshkin@outlook.com>
2024-07-18 01:14:42 +0300

    Правки

--- src/pages/manufacturers/views/indexPage.vue
+++ src/pages/manufacturers/views/indexPage.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import CrumbsComponent from '@/components/CrumbsComponent.vue'
-import { fDate } from '@/composables/useDate'
 import { useEquipments } from '@/pages/equipments/store/useEquipments'
 import { useManufacturers } from '@/pages/manufacturers/store/useManufacturers'
+import CardComponent from '@/pages/post/component/CardComponent.vue'
 import { usePosts } from '@/pages/post/store/usePosts'
 import { useRoute } from 'vue-router'
-
-const url = import.meta.env.VITE_URL
 
 const posts = usePosts()
 const equipments = useEquipments()
@@ -23,64 +21,55 @@ equipments.find()
     <CrumbsComponent :crumb="store.listOne?.name" />
 
     <section>
-      <article>
-        <h1>
-          {{ store.listOne?.name }}
-        </h1>
+      <h1>
+        {{ store.listOne?.name }}
+      </h1>
 
-        <p>{{ store.listOne?.description }}</p>
-      </article>
+      <p>{{ store.listOne?.description }}</p>
+    </section>
 
-      <article class="content" v-html="store.listOne?.content"></article>
+    <section class="content" v-html="store.listOne?.content" />
 
+    <section>
       <ul>
-        <li><h3>Оборудование производителя:</h3></li>
+        <li>Оборудование производителя:</li>
 
-        <li
-          v-for="item of equipments.list.filter((el) =>
+        <RouterLink
+          v-for="el of equipments.list.filter((el) =>
             el.manufacturers.some((e) => e.id === +route.params.id)
           )"
-          :key="item.id"
-          :class="{ active: item.description }"
-        >
-          <RouterLink
-            v-if="item.description"
-            :to="{
-              name: 'equipment',
-              params: { id: item.id }
-            }"
-          >
-            {{ item.name }}
-          </RouterLink>
-
-          <p v-else>{{ item.name }}</p>
-        </li>
-      </ul>
-
-      <div>
-        <RouterLink
-          v-for="el of posts.list
-            .filter((el) => el.manufacturers.some((e) => e.id === +route.params.id))
-            .slice(0, 2)"
-          :key="el.name"
-          :to="{
-            name: 'post',
-            params: { id: el.id }
-          }"
           custom
           v-slot="{ navigate }"
+          :key="el.id"
+          :to="{
+            name: 'equipment',
+            params: { id: el.id }
+          }"
         >
-          <article v-on:click="navigate" class="card">
-            <picture>
-              <img v-if="el.image" :src="`${url}/out/${el.image}.webp`" alt="" />
-            </picture>
+          <li v-if="el.description" v-on:click="navigate" :class="{ active: el.description }">
+            {{ el.name }}
+          </li>
 
-            <h3>{{ el.name }}</h3>
-
-            <time>{{ fDate(el.published, 'fullDate') }}</time>
-          </article>
+          <li v-else>{{ el.name }}</li>
         </RouterLink>
-      </div>
+      </ul>
+    </section>
+
+    <section>
+      <RouterLink
+        v-for="el of posts.list
+          .filter((el) => el.manufacturers.some((e) => e.id === +route.params.id))
+          .slice(0, 2)"
+        :key="el.name"
+        :to="{
+          name: 'post',
+          params: { id: el.id }
+        }"
+        custom
+        v-slot="{ navigate }"
+      >
+        <CardComponent :data="el" v-on:click="navigate" />
+      </RouterLink>
     </section>
   </main>
 </template>
@@ -88,66 +77,63 @@ equipments.find()
 <style scoped lang="scss">
 main {
   display: grid;
+  grid-template: auto / repeat(2, 1fr);
   gap: var(--theme-gap);
 
+  nav {
+    grid-column: 1 / -1;
+  }
+
   section {
-    display: grid;
-    grid-template: auto / repeat(2, 1fr);
-    gap: var(--theme-gap);
+    background-color: rgb(var(--color-light));
+    padding: clamp(20px, 2vw, 40px);
 
-    article {
-      background-color: rgb(var(--color-light));
-      padding: clamp(20px, 2vw, 40px);
-
-      &:first-of-type {
-        grid-column: 1 / -1;
-      }
+    &:first-of-type {
+      grid-column: 1 / -1;
     }
 
     ul {
-      background-color: rgb(var(--color-light));
-      padding: clamp(20px, 2vw, 40px);
-
       li {
+        &:first-of-type {
+          font-weight: 400;
+          font-size: 20px;
+        }
+
         &.active {
           &:hover {
             color: rgb(var(--color-theme));
             cursor: pointer;
-            text-decoration: none;
           }
         }
       }
     }
 
-    div {
-      background-color: rgb(var(--color-light));
+    &:last-of-type {
       display: grid;
       gap: 20px;
       grid-column: 1 / -1;
       grid-template: auto / repeat(4, 1fr);
-      padding: clamp(20px, 2vw, 40px);
+    }
+  }
+}
 
-      article {
-        background-color: #ffffff;
-        grid-column: initial !important;
-        padding: 20px;
+@media only screen and (max-width: 1500px) {
+  main {
+    section {
+      &:last-of-type {
+        grid-template: auto / 1fr 1fr;
+      }
+    }
+  }
+}
 
-        picture {
-          overflow: hidden;
+@media only screen and (max-width: 720px) {
+  main {
+    grid-template: auto / 1fr;
 
-          img {
-            display: block;
-            transition: 0.2s ease-in-out;
-          }
-        }
-
-        &:hover {
-          picture {
-            img {
-              transform: scale(1.2);
-            }
-          }
-        }
+    section {
+      &:last-of-type {
+        grid-template: auto / 1fr;
       }
     }
   }
